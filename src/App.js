75742170f2990dffe9c916dd3816929c29361202
SaadSamee-123 <saadsameequreshi@gmail.com>
2022-10-03 13:56:21 +0500

    table-assignmnt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    setPosts(response.data);
+  };
+
+  const deletePost = async (id) => {
+    const data = posts.filter((e) => e.id !== id);
+    setPosts(data);
+    const response = await axios.delete(
+      `https://jsonplaceholder.typicode.com/posts/${id}`
+    );
+    console.log(response);
+  };
+
+  const updatePost = async (data) => {
+    const response = await axios.put(
+      `https://jsonplaceholder.typicode.com/posts/${data.id}`,
+      data
+    );
+    console.log(response);
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((p, i) => {
+            return (
+              <tr key={p.id}>
+                <td>{i + 1}</td>
+                <td>{p.title}</td>
+                <td>{p.body}</td>
+                <td>
+                  <button
+                    onClick={() => updatePost(p)}
+                    type="button"
+                    className="btn btn-primary mb-2"
+                  >
+                    Update
+                  </button>
+                  <br />
+                  <button
+                    onClick={() => deletePost(p.id)}
+                    type="button"
+                    className="btn btn-secondary"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
